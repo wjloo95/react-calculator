8f4dcb2d38eeff6c26545707090f3d128127893c
wjloo95 <loo.williamj@gmail.com>
2020-04-19 17:31:53 -0400

    Initialize Calculator Component

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
 interface Props {}
-interface State {}
-export default class Calculator extends Component {
-  render() {
-    return <div></div>;
+interface State {
+  displayValue?: string;
+  numbers?: number[];
+  operators?: string[];
+  selectedOperator?: string;
+  storedValue?: string;
+}
+
+const initialState = {
+  displayValue: '0',
+  numbers: [],
+  operators: [],
+  selectedOperator: '',
+  storedValue: '',
+};
+export default class Calculator extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = initialState;
   }
+  callOperator = () => {
+    console.log('call operation');
+  };
+
+  setOperator = () => {
+    console.log('set operation');
+  };
+
+  updateDisplay = () => {
+    console.log('update display');
+  };
+
+  render = () => {
+    return <div className="calculator-container" />;
+  };
 }
