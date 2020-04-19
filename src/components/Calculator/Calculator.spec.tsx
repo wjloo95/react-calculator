b8fcb59eada2dbf46a1185efad1eff7a45830170
wjloo95 <loo.williamj@gmail.com>
2020-04-19 17:54:33 -0400

    Add Display tests

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 import Calculator from './Calculator';
+import Display from '../Display/Display';
 
 describe('Calculator', () => {
-  let wrapper: ShallowWrapper;
+  let wrapper: any;
 
   beforeEach(() => (wrapper = shallow(<Calculator />)));
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render the Display Component', () => {
+    expect(wrapper)
+      .containsMatchingElement(<Display />)
+      .toEqual(true);
   });
 });
