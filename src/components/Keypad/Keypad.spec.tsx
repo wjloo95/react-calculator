9e18dc5ae2102f606dd94bb6f07e3181c72c6be1
wjloo95 <loo.williamj@gmail.com>
2020-04-19 20:21:24 -0400

    Create keypad render test

---
+++ src/components/Keypad/Keypad.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Keypad from './Keypad';
+
+describe('Keypad', () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Keypad
+        callOperator={jest.fn()}
+        numbers={[]}
+        operators={[]}
+        setOperator={jest.fn()}
+        updateDisplay={jest.fn()}
+      />
+    );
+  });
+
+  it('should render a div', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+});
