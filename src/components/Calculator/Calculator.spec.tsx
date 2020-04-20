ff171ce2daf3cf454c1cc3a90b7437cd42efa407
wjloo95 <loo.williamj@gmail.com>
2020-04-20 01:11:11 -0400

    Add callOperator tests

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -150,4 +150,81 @@ describe('mounted Calculator', () => {
       expect(wrapper.state('storedValue')).toEqual('5');
     });
   });
+
+  describe('callOperator', () => {
+    let wrapper: any;
+
+    beforeEach(() => (wrapper = shallow(<Calculator />)));
+
+    it('should update displayValue to the sum of storedValue and displayValue', () => {
+      wrapper.setState({ storedValue: '3' });
+      wrapper.setState({ displayValue: '2' });
+      wrapper.setState({ selectedOperator: '+' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('5');
+    });
+
+    it('should update displayValue to the difference of storedValue and displayValue', () => {
+      wrapper.setState({ storedValue: '3' });
+      wrapper.setState({ displayValue: '2' });
+      wrapper.setState({ selectedOperator: '-' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('1');
+    });
+
+    it('should update displayValue to the product of storedValue and displayValue', () => {
+      wrapper.setState({ storedValue: '3' });
+      wrapper.setState({ displayValue: '2' });
+      wrapper.setState({ selectedOperator: 'x' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('6');
+    });
+
+    it('should update displayValue to the quotient of storedValue and displayValue', () => {
+      wrapper.setState({ storedValue: '3' });
+      wrapper.setState({ displayValue: '2' });
+      wrapper.setState({ selectedOperator: '/' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('1.5');
+    });
+
+    it('should update displayValue to 0 if operation results in NaN', () => {
+      wrapper.setState({ storedValue: '3' });
+      wrapper.setState({ displayValue: 'string' });
+      wrapper.setState({ selectedOperator: '/' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+
+    it('should update displayValue to 0 if operation results in Infinity', () => {
+      wrapper.setState({ storedValue: '7' });
+      wrapper.setState({ displayValue: '0' });
+      wrapper.setState({ selectedOperator: '/' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+
+    it('should update displayValue to 0 if selectedOperator does not match given operators', () => {
+      wrapper.setState({ storedValue: '7' });
+      wrapper.setState({ displayValue: '10' });
+      wrapper.setState({ selectedOperator: 'string' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+
+    it('should update displayValue to 0 if called with no value for storedValue', () => {
+      wrapper.setState({ storedValue: '' });
+      wrapper.setState({ displayValue: '10' });
+      wrapper.setState({ selectedOperator: '+' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+    it('should update displayValue to 0 if called with no value for selectedOperator', () => {
+      wrapper.setState({ storedValue: '12' });
+      wrapper.setState({ displayValue: '10' });
+      wrapper.setState({ selectedOperator: '' });
+      wrapper.instance().callOperator();
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+  });
 });
