4402cb1cd32c76373ea7b894dd063e0fa8030743
wjloo95 <loo.williamj@gmail.com>
2020-04-20 00:51:29 -0400

    Add tests to check updateDisplay function

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -69,4 +69,52 @@ describe('mounted Calculator', () => {
     wrapper.find('.submit-key').simulate('click');
     expect(spy).toHaveBeenCalledTimes(1);
   });
+
+  describe('updateDisplay', () => {
+    let wrapper: any;
+
+    beforeEach(() => (wrapper = shallow(<Calculator />)));
+
+    it('should update displayValue', () => {
+      wrapper.instance().updateDisplay('5');
+      expect(wrapper.state('displayValue')).toEqual('5');
+    });
+
+    it('should concatenate displayValue', () => {
+      wrapper.instance().updateDisplay('5');
+      wrapper.instance().updateDisplay('0');
+      expect(wrapper.state('displayValue')).toEqual('50');
+    });
+
+    it('should remove leading 0 from displayValue', () => {
+      wrapper.instance().updateDisplay('0');
+      expect(wrapper.state('displayValue')).toEqual('0');
+      wrapper.instance().updateDisplay('5');
+      expect(wrapper.state('displayValue')).toEqual('5');
+    });
+
+    it('should prevent multiple leading 0s from displayValue', () => {
+      wrapper.instance().updateDisplay('0');
+      wrapper.instance().updateDisplay('0');
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+
+    it('should remove last char of displayValue', () => {
+      wrapper.instance().updateDisplay('5');
+      wrapper.instance().updateDisplay('0');
+      wrapper.instance().updateDisplay('ce');
+      expect(wrapper.state('displayValue')).toEqual('5');
+    });
+
+    it('should prevent multiple instances of . in displayValue', () => {
+      wrapper.instance().updateDisplay('.');
+      wrapper.instance().updateDisplay('.');
+      expect(wrapper.state('displayValue')).toEqual('.');
+    });
+
+    it('should set displayValue to 0 if displayValue is equal to an empty string', () => {
+      wrapper.instance().updateDisplay('ce');
+      expect(wrapper.state('displayValue')).toEqual('0');
+    });
+  });
 });
