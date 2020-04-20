876e24f2d409e2938a157b4dbd4c75163e6a6385
wjloo95 <loo.williamj@gmail.com>
2020-04-19 20:14:08 -0400

    Add calculator keypad tests

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import Calculator from './Calculator';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
   let wrapper: any;
@@ -17,6 +18,20 @@ describe('Calculator', () => {
     expect(
       wrapper.containsMatchingElement(
         <Display displayValue={wrapper.instance().state.displayValue} />
+      )
+    ).toEqual(true);
+  });
+
+  it('should render the Keypad Component', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <Keypad
+          callOperator={wrapper.instance().callOperator}
+          numbers={wrapper.instance().state.numbers}
+          operators={wrapper.instance().state.operators}
+          setOperator={wrapper.instance().setOperator}
+          updateDisplay={wrapper.instance().updateDisplay}
+        />
       )
     ).toEqual(true);
   });
